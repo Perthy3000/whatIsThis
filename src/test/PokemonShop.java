441b118b28bd33454e933645b75fffe9148be6cd
umimchoi <54405930+umimchoi@users.noreply.github.com>
2019-11-18 21:57:17 +0700

    Add files via upload

---
+++ src/test/PokemonShop.java
@@ -0,0 +1,20 @@
+package test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class PokemonShop {
+List<test1>pokemons;
+	public PokemonShop() {
+	prompt();
+}
+	public void prompt() {
+		System.out.println("Select Pokemon :");
+		System.out.println("1: Magikarp");
+		System.out.println("2: Choi, The Genius");
+		System.out.println("3: Perth3000, The God");
+		System.out.println("4: Exit Shop");
+	}
+
+}
