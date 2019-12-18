4b339410f6e881ab42a159cc9e1c1a05a0f7cb82
Natnaree Panya <umimchoi@gmail.com>
2019-12-18 20:03:48 +0700

    Choi's Part is done

--- src/gui/Blackmarket.java
+++ src/gui/Blackmarket.java
@@ -7,14 +7,21 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundImage;
 import javafx.scene.layout.BackgroundPosition;
 import javafx.scene.layout.BackgroundRepeat;
 import javafx.scene.layout.BackgroundSize;
+import javafx.scene.layout.Border;
+import javafx.scene.layout.BorderStroke;
+import javafx.scene.layout.BorderStrokeStyle;
+import javafx.scene.layout.BorderWidths;
+import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
 import pokemon.test1;
@@ -27,7 +34,7 @@ public class Blackmarket extends VBox {
 	private PokemonShop shop;
 	private Buyinglog shoplog;
 	private guiPokeball pokelog;
-	
+	private Image buttonImage;
 	public Blackmarket(Player player1, Stage primaryStage) {
 		Image image = new Image ("marketbg.png");
 	    setBackground(new Background(new BackgroundImage(image,BackgroundRepeat.REPEAT,
@@ -55,9 +62,13 @@ public class Blackmarket extends VBox {
 		
 		this.getChildren().add(pokeball);
 		Button exitButton = new Button();
-		exitButton.setText("Exit");
-		exitButton.setStyle("-fx-font-size: 15");
+
 		exitButton.setPrefSize(70, 50);
+		exitButton.setBorder(new Border(new BorderStroke(Color.TRANSPARENT,BorderStrokeStyle.NONE, 
+				CornerRadii.EMPTY, BorderWidths.EMPTY)));
+		exitButton.setStyle("-fx-background-color: transparent;");
+		buttonImage = new Image(ClassLoader.getSystemResource("exitbf.png").toString(), 70, 42, false, false);
+		exitButton.setGraphic(new ImageView(buttonImage));
 		HBox exit =  new HBox();
 		exit.setAlignment(Pos.BOTTOM_CENTER);
 		exit.getChildren().add(exitButton);
@@ -67,6 +78,18 @@ public class Blackmarket extends VBox {
 			public void handle(MouseEvent arg0) {
 				primaryStage.setScene(new Scene(new MainMenuScene(primaryStage, player1), 500, 800));
 				primaryStage.setTitle("Main Menu");
+			}
+		});
+		exitButton.setOnMouseEntered(new EventHandler<MouseEvent>() {
+			@Override
+			public void handle(MouseEvent arg0) {
+				exitButton.setGraphic(new ImageView(new Image(ClassLoader.getSystemResource("exitaf.png").toString(), 70, 42, false, false)));
+			}
+		 });
+		exitButton.setOnMouseExited(new EventHandler<MouseEvent>() {
+			@Override
+			public void handle(MouseEvent arg0) {
+				exitButton.setGraphic(new ImageView(new Image(ClassLoader.getSystemResource("exitbf.png").toString(), 70, 42, false, false)));
 			}
 		});
 		this.getChildren().add(exit);
